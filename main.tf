50ce01fa6347e457c5f596677748e55074f2e753
Job Céspedes Ortiz <jobcespedes@gmail.com>
2023-08-23 10:21:06 -0600

    fix: use new vars

--- main.tf
+++ main.tf
@@ -14,20 +14,23 @@ module "maddy" {
   )
 
   # butane common
-  fqdn               = var.fqdn
-  cidr_ip_address    = var.cidr_ip_address
-  mac                = var.mac
-  ssh_authorized_key = var.ssh_authorized_key
-  nameservers        = var.nameservers
-  timezone           = var.timezone
-  rollout_wariness   = var.rollout_wariness
-  periodic_updates   = var.periodic_updates
-  keymap             = var.keymap
-  autostart          = var.autostart
-  etc_hosts_extra    = var.etc_hosts_extra
+  fqdn                = var.fqdn
+  cidr_ip_address     = var.cidr_ip_address
+  mac                 = var.mac
+  ssh_authorized_key  = var.ssh_authorized_key
+  nameservers         = var.nameservers
+  timezone            = var.timezone
+  rollout_wariness    = var.rollout_wariness
+  periodic_updates    = var.periodic_updates
+  keymap              = var.keymap
+  autostart           = var.autostart
+  etc_hosts_extra     = var.etc_hosts_extra
+  interface_name      = var.interface_name
+  sync_time_with_host = var.sync_time_with_host
   # libvirt
   vcpu                  = var.vcpu
   memory                = var.memory
+  machine               = var.machine
   root_base_volume_name = var.root_base_volume_name
   root_base_volume_pool = var.root_base_volume_pool
   data_volume_pool      = var.data_volume_pool
